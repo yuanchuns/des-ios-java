7cdd32139cd442e5c84cf0842f50f56f00c7e5bd
nextone <yuanchu_n@163.com>
2015-08-26 14:53:30 +0800

    des加密iOS  java
    des加密iOS  java

--- des/ViewController.m
+++ des/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "GTMBase64.h"
+#include <CommonCrypto/CommonCryptor.h>
 
 @interface ViewController ()
 
@@ -18,12 +20,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // Release any retained subviews of the main view.
+    // 設定 key
+    NSString* key = @"abcdefgh";
+    NSString* encrypt = @"aR/T8qHQ4uY=";
+    NSString* decrypt = [self decryptUseDES:encrypt key:key];
+    
+    // 輸出
+    NSLog(@"encrypt = %@", encrypt);
+    NSLog(@"decrypt = %@", decrypt);
+    encrypt = [self encryptUseDES:@"我们都是好孩子" key:key];
+    NSLog(@"encrypt = %@", encrypt);
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -33,6 +46,60 @@
     } else {
         return YES;
     }
+}
+-(NSString*) decryptUseDES:(NSString*)cipherText key:(NSString*)key {
+    // 利用 GTMBase64 解碼 Base64 字串
+    NSData* cipherData = [GTMBase64 decodeString:cipherText];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    size_t numBytesDecrypted = 0;
+    
+    // IV 偏移量不需使用
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                          [key UTF8String],
+                                          kCCKeySizeDES,
+                                          nil,
+                                          [cipherData bytes],
+                                          [cipherData length],
+                                          buffer,
+                                          1024,
+                                          &numBytesDecrypted);
+    NSString* plainText = nil;
+    if (cryptStatus == kCCSuccess) {
+        NSData* data = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesDecrypted];
+        plainText = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+    }
+    return plainText;
+}
+-(NSString *) encryptUseDES:(NSString *)clearText key:(NSString *)key
+{
+    NSData *data = [clearText dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    size_t numBytesEncrypted = 0;
+    
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt,
+                                          kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                          [key UTF8String],
+                                          kCCKeySizeDES,
+                                          nil,
+                                          [data bytes],
+                                          [data length],
+                                          buffer,
+                                          1024,
+                                          &numBytesEncrypted);
+    
+    NSString* plainText = nil;
+    if (cryptStatus == kCCSuccess) {
+        NSData *dataTemp = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesEncrypted];
+        plainText = [GTMBase64 stringByEncodingData:dataTemp];
+    }else{
+        NSLog(@"DES加密失败");
+    }
+    return plainText;
 }
 
 @end
